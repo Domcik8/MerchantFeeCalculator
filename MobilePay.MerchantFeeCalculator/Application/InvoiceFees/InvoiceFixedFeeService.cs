a0717a9c52f7dc6c13ea687bce73e31c355c0800
Dominik Lisovski <Dominik.Lisovski@gmail.com>
2020-06-25 18:58:52 +0300

    Add invoice fixed fees

---
+++ MobilePay.MerchantFeeCalculator/Application/InvoiceFees/InvoiceFixedFeeService.cs
@@ -0,0 +1,27 @@
+﻿using Domain;
+
+namespace Application.TransactionFees
+{
+    public class InvoiceFixedFeeService : BaseMerchantFeeService
+    {
+        public const decimal StandardInvoiceFixedFee = 29;
+
+        public override void CalculateMerchantFee(Transaction transaction)
+        {
+            CalculateInvoiceFixedFee(transaction);
+        }
+
+        private void CalculateInvoiceFixedFee(Transaction transaction)
+        {
+            if (IsFreeTransaction(transaction))
+                return;
+
+            transaction.Fee += StandardInvoiceFixedFee;
+        }
+
+        private bool IsFreeTransaction(Transaction transaction)
+        {
+            return transaction.Fee <= 0;
+        }
+    }
+}
---
+++ MobilePay.MerchantFeeCalculator/Tests/Application.UnitTests/InvoiceFixedFeeServiceTests.cs
@@ -0,0 +1,47 @@
+﻿using Application.TransactionFees;
+using Domain;
+using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Application.UnitTests
+{
+    public class InvoiceFixedFeeServiceTests
+    {
+        [Theory]
+        [InlineData(120)]
+        [InlineData(10)]
+        [InlineData(300)]
+        public void CalculateMerchantFee_ForNonFreeTransaction_ShouldIncludeInvoiceFee(decimal fee)
+        {
+            // Arrange
+            var transaction = new Transaction { Fee = fee };
+            var sut = new InvoiceFixedFeeService();
+            var expected = fee + InvoiceFixedFeeService.StandardInvoiceFixedFee;
+
+            // Act
+            sut.CalculateMerchantFee(transaction);
+            var actual = transaction.Fee;
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        public void CalculateMerchantFee_ForFreeTransaction_ShouldNotIncludeInvoiceFee(decimal fee)
+        {
+            // Arrange
+            var transaction = new Transaction { Fee = fee };
+            var sut = new InvoiceFixedFeeService();
+            var expected = 0;
+
+            // Act
+            sut.CalculateMerchantFee(transaction);
+            var actual = transaction.Fee;
+
+            // Assert
+            actual.Should().Be(expected);
+        }
+    }
+}
