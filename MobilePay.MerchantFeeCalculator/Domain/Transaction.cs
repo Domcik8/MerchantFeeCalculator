00a35b5c047c474b87b1b3b724159d0c00752107
Dominik Lisovski <Dominik.Lisovski@gmail.com>
2020-06-25 11:19:23 +0300

    Add Transaction domain object

---
+++ MobilePay.MerchantFeeCalculator/Domain/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain
+{
+    class Transaction
+    {
+        public DateTime Date { get; set; }
+        public string MerchantName { get; set; }
+        public string Amount { get; set; }
+        public string Fee { get; set; }
+    }
+}
